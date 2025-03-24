f03dc39b3879ba25b74de3e39bbdc560f6d9399a
Bookseal <Gichanlee@icloud.com>
2025-03-24 18:56:34 +0900

    add find_ft_type.py and add format_ft_time.py

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -1,5 +1,9 @@
 import time
+from datetime import datetime
 
 current_time = time.time()
 
-print(f"Seconds since January 1, 1970: {current_time:4f,.4f}")
+print(f"Seconds since January 1, 1970: {current_time:,.4f} or {current_time:e} in scientific notation")
+
+current_date = datetime.now()
+print(f"{current_date:%b %d %Y}")
---
+++ ex02/find_ft_type.py
@@ -0,0 +1,35 @@
+def all_thing_is_obj(object: any) -> int:
+    object_type = type(object)
+    result = ""
+    return_num = 42
+
+    if (object_type == list):
+        result += "List :"
+    elif (object_type == tuple):
+        result += "Tuple :"
+    elif (object_type == set):
+        result += "Set :"
+    elif (object_type == dict):
+        result += "Dict :"
+    elif (object_type == str):
+        result += object + " is in the kitchen :"
+    else:
+        print("Type not found")
+        return return_num
+
+    print(result, type(object))
+    return return_num
+
+
+ft_list = ["Hello", "tata!"]
+ft_tuple = ("Hello", "toto!")
+ft_set = {"Hello", "tutu!"}
+ft_dict = {"Hello": "titi!"}
+
+all_thing_is_obj(ft_list)
+all_thing_is_obj(ft_tuple)
+all_thing_is_obj(ft_set)
+all_thing_is_obj(ft_dict)
+all_thing_is_obj("Brian")
+all_thing_is_obj("Toto")
+print(all_thing_is_obj(10))
