473d728405900b6270f00fc9b6e27da2f03a70ab
Bookseal <Gichanlee@icloud.com>
2025-03-27 21:59:22 +0900

    add NULL_not_found.py

---
+++ ex03/NULL_not_found.py
@@ -0,0 +1,38 @@
+import types
+import math
+
+def NULL_not_found(object: any) -> int:
+    obj_type = type(object)
+    return_value = 0
+
+    output_message = ""
+
+    if (obj_type is types.NoneType):
+        output_message += "Nothing: None <class 'NoneType'>"
+    elif (obj_type is float and math.isnan(object)):
+        output_message += "Cheese: nan <class 'float'>"
+    elif (obj_type is int and object == 0):
+        output_message += "Zero: 0 <class 'int'>"
+    elif (obj_type is str and object == ""):
+        output_message += "Empty: <class 'str'>"
+    elif (obj_type is bool):
+        output_message += "Fake: False <class 'bool'>"
+    else:
+        output_message += "Type not found"
+        return_value = 1
+    print(output_message)
+    return return_value
+
+
+Nothing = None
+Garlic = float("NaN")
+Zero = 0
+Empty =  ""
+Fake = False
+
+NULL_not_found(Nothing)
+NULL_not_found(Garlic)
+NULL_not_found(Zero)
+NULL_not_found(Empty)
+NULL_not_found(Fake)
+print(NULL_not_found("Brian"))
