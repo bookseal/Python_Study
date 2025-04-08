cd510f30251cfb84e7409193a8aeb00c0d48fccf
bookseal <116708901+bookseal@users.noreply.github.com>
2025-04-08 12:56:41 +0000

    fix: update scientific notation format in time output

--- ex01/format_ft_time.py
+++ ex01/format_ft_time.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 current_time = time.time()
 
-print(f"Seconds since January 1, 1970: {current_time:,.4f} or {current_time:e} in scientific notation")
+print(f"Seconds since January 1, 1970: {current_time:,.4f} or {current_time:.2e} in scientific notation")
 
 current_date = datetime.now()
 print(f"{current_date:%b %d %Y}")
