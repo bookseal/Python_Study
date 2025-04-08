00708e7ac1ad0ecc89d0da2fc20a970367097080
bookseal <116708901+bookseal@users.noreply.github.com>
2025-04-08 13:34:37 +0000

    fix: update docstring in ft_filter function for clarity

--- ex06/filterstring.py
+++ ex06/filterstring.py
@@ -2,6 +2,13 @@ import sys
 from ft_filter import ft_filter 
 
 def main(argc, argv):
+    '''
+    This program filters a string based on the length of its words.
+    It takes two arguments:
+    1. A string containing words separated by spaces.
+    2. An integer representing the minimum length of words to keep.
+    The program will print a list of words that are longer than the specified length.
+    '''
     # Assert that the number of arguments is correct
     assert argc == 3, "AssertionError: The program requires exactly 2 arguments.\nUsage: python filterstring.py '<string>' <number>'"
 
@@ -24,5 +31,7 @@ def main(argc, argv):
         raise AssertionError("AssertionError: The second argument must be an integer.")
 
 if __name__ == "__main__":
+    # print(filter.__doc__)
+    # print(ft_filter.__doc__)
     main(len(sys.argv), sys.argv)
 
--- ex06/ft_filter.py
+++ ex06/ft_filter.py
@@ -1,14 +1,10 @@
 def ft_filter(function, iterable):
-    """Filter elements from iterable based on function.
-    
-    Args:
-        function: A function that returns True or False (can be None)
-        iterable: Any iterable collection (list, tuple, etc.)
-    
-    Returns:
-        A list containing items from iterable where function(item) is True.
-        If function is None, returns items that evaluate to True.
-    """
+    '''
+filter(function or None, iterable) --> filter object
+
+Return an iterator yielding those items of iterable for which function(item)
+is true. If function is None, return the items that are true.
+    '''
     if function is not None:
         return [item for item in iterable if function(item)]
     else:
