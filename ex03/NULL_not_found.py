21a8b1af8c1c7a37ad410de9ddb1cf3516c01364
bookseal <116708901+bookseal@users.noreply.github.com>
2025-04-08 13:07:59 +0000

    refactor: simplify NULL_not_found function and clean up comments

--- ex03/NULL_not_found.py
+++ ex03/NULL_not_found.py
@@ -1,21 +1,18 @@
-import types
-import math
-
 def NULL_not_found(object: any) -> int:
     obj_type = type(object)
     return_value = 0
 
     output_message = ""
 
-    if (obj_type is types.NoneType):
+    if object is None:
         output_message += "Nothing: None <class 'NoneType'>"
-    elif (obj_type is float and math.isnan(object)):
+    elif obj_type is float and object != object:  # NaN check: NaN is not equal to itself
         output_message += "Cheese: nan <class 'float'>"
-    elif (obj_type is int and object == 0):
+    elif obj_type is int and object == 0:
         output_message += "Zero: 0 <class 'int'>"
-    elif (obj_type is str and object == ""):
+    elif obj_type is str and object == "":
         output_message += "Empty: <class 'str'>"
-    elif (obj_type is bool):
+    elif obj_type is bool and object is False:
         output_message += "Fake: False <class 'bool'>"
     else:
         output_message += "Type not found"
@@ -24,15 +21,15 @@ def NULL_not_found(object: any) -> int:
     return return_value
 
 
-Nothing = None
-Garlic = float("NaN")
-Zero = 0
-Empty =  ""
-Fake = False
+# Nothing = None
+# Garlic = float("NaN")
+# Zero = 0
+# Empty = ""
+# Fake = False
 
-NULL_not_found(Nothing)
-NULL_not_found(Garlic)
-NULL_not_found(Zero)
-NULL_not_found(Empty)
-NULL_not_found(Fake)
-print(NULL_not_found("Brian"))
+# NULL_not_found(Nothing)
+# NULL_not_found(Garlic)
+# NULL_not_found(Zero)
+# NULL_not_found(Empty)
+# NULL_not_found(Fake)
+# print(NULL_not_found("Brian"))
